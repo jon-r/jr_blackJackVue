08a8ca5a7adaef7380fee60c1cbeb2b5fde7d44a
Jon Richards <jonr.web@gmail.com>
2024-09-11 10:33:28 +0100

    have the lint work properly with vue files

--- eslint.config.js
+++ eslint.config.js
@@ -3,33 +3,32 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import pluginVue from "eslint-plugin-vue";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import vueParser from "vue-eslint-parser";
 
 // todo strictest typescript? https://gist.github.com/dilame/32709f16e3f8d4d64b596f5b19d812e1?permalink_comment_id=4708061#gistcomment-4708061
 export default [
   { ignores: ["dist"] },
-  { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,vue}"],
+    linterOptions: {
+      reportUnusedDisableDirectives: "error",
+    },
+  },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
   {
     files: ["**/*.js"],
     ...tseslint.configs.disableTypeChecked,
   },
   ...pluginVue.configs["flat/recommended"],
   {
-    files: ["**/*.vue"],
     languageOptions: {
-      parser: vueParser,
-      parserOptions: { parser: tseslint.parser },
+      parserOptions: {
+        parser: tseslint.parser,
+        projectService: true,
+        extraFileExtensions: [".vue"],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
   eslintConfigPrettier,
